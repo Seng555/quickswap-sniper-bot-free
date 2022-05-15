8d2d64ab627babb89b369693d4860b32f856978e
Fabitnex <eeyang5@gmail.com>
2022-05-15 12:57:48 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
-walletAddress = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"                     #Your Wall address From trustwallet or MetaMask or another wallet.
-private_key = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270" #Wallet private_key
+walletAddress = "Your_wallet"                     #Your Wall address From trustwallet or MetaMask or another wallet.
+private_key = "Wallet_private_key" #Wallet private_key
 
 spend = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"  # WMATIC OR BUSD OR USDT OR OTHER (Default MATIC) contract for buy the token
 
@@ -12,9 +12,9 @@ Takeprofit = 250  # In percent
 
 transactionRevertTime = 1000 #Limit for make transaction
 gasAmount = 500000 #Minimul limit is 210000, more much more better.
-gasPrice = 7 #Customize your GWEI (gas fee) here, cannot decimal. (eg : 5, 10, 25).
+gasPrice = 50 #Customize your GWEI (gas fee) here, cannot decimal. (eg : 5, 10, 25).
 
 quickswapRouterAddress = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff"
 quickswapFactoryAddress = "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32"
 
-polygon_rpc = "https://polygon-rpc.com"          #BSC JSON-RPC
+polygon_rpc = "https://polygon-rpc.com"          #POLYGON JSON-RPC
